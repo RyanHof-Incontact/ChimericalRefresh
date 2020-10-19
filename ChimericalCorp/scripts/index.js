557ad5de8a2fd0a517653e820ba91e95374a2c1a
RyanHof-Incontact <54338237+RyanHof-Incontact@users.noreply.github.com>
2020-10-19 15:19:16 -0400

    Test
    Test

--- ChimericalCorp/scripts/index.js
+++ ChimericalCorp/scripts/index.js
@@ -14,6 +14,7 @@ const setupUI = (user) => {
     db.collection('users').doc(user.uid).get().then(doc => {
       const html = `
           <div>Logged in as ${user.email}</div>
+          console.log(C32 Agent Id: ${doc.data().c32agentID});
           <div>C32 Agent Id: ${doc.data().c32agentID}</div>
         `;
         accountDetails.innerHTML = html;
@@ -43,7 +44,7 @@ if (data.length){
       const li = `
         <li>
           <div class="collapsible-header grey lighten-4">${guide.title}</div>
-          <div class="collapsible-body white">${doc.data().c32agentID}</div>
+          <div class="collapsible-body white">C32 Agent ID: ${doc.data().c32agentID}</div>
           <div class="collapsible-body white">${guide.environment}</div>
         </li>
       `;
